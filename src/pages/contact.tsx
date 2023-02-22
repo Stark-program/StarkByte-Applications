40db6b6fcd1c7583265a64a9a87894b43ae264d4
Stark-program <71916880+Stark-program@users.noreply.github.com>
2023-02-21 21:24:33 -0700

    Revert "Revert "Merge Testing to Master""

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,13 +1,44 @@
+<<<<<<< HEAD
+import { useState } from "react";
+import SuccessfulEmail from "../components/SuccessfulEmail";
+import Pageillustration from "../components/PageIllustration";
+import Background from "../components/Background";
+=======
 import React from "react";
+>>>>>>> 6e70cd66edbcdd7f829ad2631910ad8e7a407327
 
 const ContactPage = () => {
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = async (data: ContactPageProps) => {
+    try {
+      const sendEmail = await fetch("/api/email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const response = await sendEmail.json();
+      console.log(response);
+      if (response.status === 201) {
+        setSuccess(true);
+        setMessage("");
+        setEmail("");
+        setSubject("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <section className="h-screen bg-gray-900">
-      <div className="mx-auto max-w-screen-md py-8 px-4 lg:py-16">
-        {/* <h2 className="mb-4 text-center text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
-          Send us an email for a free consultation
-        </h2> */}
-        <p className="mb-8 text-center font-light text-gray-400 sm:text-xl lg:mb-16">
+    <section className="h-screen overflow-hidden bg-gray-900">
+      <div className="relative z-10 mx-auto max-w-screen-md py-8 px-4 lg:py-16">
+        <p className="mb-8 text-center text-2xl font-light text-gray-400 md:text-xl lg:mb-16">
           We'd love to hear about your software ideas and how we can help turn
           them into reality. Please don't hesitate to reach out to us to get the
           conversation started.
@@ -23,8 +54,15 @@ const ContactPage = () => {
             <input
               type="email"
               id="email"
+<<<<<<< HEAD
+              className="dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
+              placeholder="example@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+=======
               className="dark:shadow-sm-light focus:border-primary-500 focus:ring-primary-500 dark:focus:border-primary-500 dark:focus:ring-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
               placeholder="name@example.com"
+>>>>>>> 6e70cd66edbcdd7f829ad2631910ad8e7a407327
               required
             />
           </div>
@@ -40,6 +78,8 @@ const ContactPage = () => {
               id="subject"
               className="dark:shadow-sm-light focus:border-primary-500 focus:ring-primary-500 dark:focus:border-primary-500 dark:focus:ring-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
               placeholder="Let us know how we can help you"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
               required
             />
           </div>
@@ -55,9 +95,12 @@ const ContactPage = () => {
               rows={6}
               className="focus:border-primary-500 focus:ring-primary-500 dark:focus:border-primary-500 dark:focus:ring-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
               placeholder="Write your message here..."
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
             ></textarea>
           </div>
           <button
+            onClick={() => handleSubmit({ email, subject, message })}
             type="submit"
             className="hover:bg-primary-800 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 rounded-lg bg-indigo-500 py-3 px-5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 sm:w-fit"
           >
@@ -65,8 +108,15 @@ const ContactPage = () => {
           </button>
         </form>
       </div>
+      {success && <SuccessfulEmail />}
     </section>
   );
 };
+
+interface ContactPageProps {
+  email: string;
+  subject: string;
+  message: string;
+}
 
 export default ContactPage;
--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -35,6 +35,15 @@ const ServicesPage = () => {
   return (
     <section className=" bg-gray-900 lg:h-screen">
       <div className="mx-auto max-w-screen-xl py-8 px-4 lg:py-16 lg:px-6">
+<<<<<<< HEAD
+        <div className=" relative z-10 mx-auto mb-8 max-w-screen-md text-center lg:mb-12">
+          <p className=" mb-5 font-playfair text-2xl font-light text-gray-300 md:text-xl">
+            StarkByte Applications provides affordable and reliable application
+            solutions tailored to the specific needs of small businesses. Our
+            direct and focused approach empowers our clients with the tools and
+            resources they need to succeed in today's competitive business
+            environment.
+=======
         <div className="mx-auto mb-8 max-w-screen-md text-center lg:mb-12">
           <h2 className="font-playfair mb-4 text-4xl font-extrabold tracking-tight text-white">
             STARK SOLUTION SERVICES
@@ -44,6 +53,7 @@ const ServicesPage = () => {
             tailored to the specific needs of small businesses. Our direct and
             focused approach empowers our clients with the tools and resources
             they need to succeed in today's competitive business environment.
+>>>>>>> 6e70cd66edbcdd7f829ad2631910ad8e7a407327
           </p>
         </div>
         <div className="space-y-8 sm:gap-6 lg:grid lg:grid-cols-3 lg:space-y-0 xl:gap-10">
