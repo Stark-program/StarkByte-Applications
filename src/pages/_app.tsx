c3f7c1e06387522b714cea40daebd6c1e6ef9036
Stark-program <71916880+Stark-program@users.noreply.github.com>
2023-02-21 21:11:12 -0700

    Revert "Merge Testing to Master"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,9 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-import Background from "../components/Background";
-import Pageillustration from "../components/PageIllustration";
-import MobileHamburger from "../components/mobile/MobileHamburger";
-import { useState } from "react";
-import MobileMenu from "../components/mobile/MobileMenu";
-import Header from "../components/Header";
+
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  const [mobileMenu, setMobileMenu] = useState(false);
-
-  const handleMobileMenu = () => {
-    setMobileMenu(!mobileMenu);
-  };
-
-  return (
-    <div className="overflow-hidden bg-gray-900">
-      <div className="relative mx-auto h-0 max-w-5xl" aria-hidden="true">
-        <Pageillustration />
-      </div>
-      <MobileHamburger mobileMenu={handleMobileMenu} />
-      {mobileMenu ? (
-        <MobileMenu mobileMenu={handleMobileMenu} mobileMenuShow={mobileMenu} />
-      ) : (
-        <Header />
-      )}
-      <Background />
-      <Component className="" {...pageProps} />
-    </div>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
