29226a6c6646cd03018be6aad7a2fbf2a16c97c6
Stark-program <chad.d.stark@hotmail.com>
2023-02-23 16:14:18 -0700

    Add Head tag to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 import MobileMenu from "../components/mobile/MobileMenu";
 import Header from "../components/Header";
 import "../styles/globals.css";
-
+import Head from "next/head";
 const MyApp: AppType = ({ Component, pageProps }) => {
   const [mobileMenu, setMobileMenu] = useState(false);
 
@@ -16,27 +16,29 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 
   return (
     <div className="h-screen w-screen overflow-y-auto overflow-x-hidden scroll-smooth bg-gray-900">
-      <meta
-        name="theme-color"
-        content="#151719"
-        media="(prefers-color-scheme: light)"
-      />
-      <meta
-        name="theme-color"
-        content="#151719"
-        media="(prefers-color-scheme: dark)"
-      />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta
-        name="apple-mobile-web-app-status-bar-style"
-        content="black-translucent"
-      />
-      <meta
-        name="apple-mobile-web-app-title"
-        content="StarkByte Applications"
-      />
-      <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Head>
+        <meta
+          name="theme-color"
+          content="#151719"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="#151719"
+          media="(prefers-color-scheme: dark)"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="StarkByte Applications"
+        />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <div className="relative mx-auto h-0 max-w-5xl" aria-hidden="true">
         <Pageillustration />
       </div>
